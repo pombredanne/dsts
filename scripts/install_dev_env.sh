1b486098821b01e4450ce5daf72e172cb7073c36
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2012-09-10 12:19:53 +1000

    Removed unnecessary items from requirements

--- scripts/install_dev_env.sh
+++ scripts/install_dev_env.sh
@@ -3,8 +3,17 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 parent_dir="$(dirname "$dir")"
 
+# Install virtual environment
+virtualenv --no-site-packages ${parent_dir}
+
+# Activate virtual environment
+source ${parent_dir}/bin/activate
+
 # Install dev requirements
 pip install -r ${parent_dir}/etc/requirements.txt # Install dev requirements
 
 # Create symbolic link so testing packages can find source files
 ln -s ${parent_dir}/dsts ${parent_dir}/lib/python2.7/site-packages/dsts
+
+# Deactivate virtual environment
+deactivate
