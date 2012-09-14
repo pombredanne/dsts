a6967bafba7518a2407d973c14f8fc59f72e0412
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2012-09-14 12:56:19 +1000

    Instead of using lib symlink, files now are staged for testing

--- scripts/install_dev_env.sh
+++ scripts/install_dev_env.sh
@@ -2,20 +2,23 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 root_dir="$(dirname "$script_dir")"   # Package root dir 
-package_name=${parent_dir##*/} # Package name = package root dir
-source_dir= ${parent_dir}/${package_name} # Python source files location
+package_name=${root_dir##*/} # Package name = package root dir
+source_dir="${root_dir}/${package_name}" # Python source files location
+lib_dir="${root_dir}/lib/python2.7/site-packages/${package_name}" # Virtual env's python lib dir
+bin_dir="${root_dir}/bin" # Virtual env's python bin dir
 
 # Install virtual environment
-virtualenv --no-site-packages ${root_dir}
+virtualenv ${root_dir}
 
 # Activate virtual environment
-source ${root_dir}/bin/activate
+source ${bin_dir}/activate
 
 # Install dev requirements
 pip install -r ${root_dir}/etc/requirements.txt # Install dev requirements
 
-# Create symbolic link so testing packages can find source files
-ln -s ${source_dir} ${root_dir}/lib/python2.7/site-packages/${package_name}
+# Stage the code
+cd $root_dir
+python setup.py install
 
 # Deactivate virtual environment
 deactivate
