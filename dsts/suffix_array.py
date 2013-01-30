4e9ddd1173928a352bb5177daeec766acb638096
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2013-01-30 15:14:35 +1100

    experimenting with string search

--- dsts/suffix_array.py
+++ dsts/suffix_array.py
@@ -66,6 +66,41 @@ class SuffixArray:
         """ Returns the original string """
         return self.str
 
+    def get_pos(self, pos):
+        """ Returns the position of the suffix array element in the original string """
+        return len(self.str) - len(self.get_sarray_item(pos))
+
+    def get_sarray_item(self, i):
+        """ Returns row from suffix array at position i """
+        return self.str[self.suffix_array[i]:]
+
+    def get_sarray_item_len(self, i):
+        """ Returns row length from suffix array at position i """
+        return len(self.str[self.suffix_array[i]:])
+
+    def get_suffix_array(self):
+        """ Returns the suffix array """
+        tmp_array = []
+        for i in self.suffix_array:
+            tmp_array.append(self.str[i:])
+        return tmp_array
+
+    def add_to_suffix_array(self, substr):
+        """ Adds a substring to the suffix array. Requires resorting the
+            suffix array and recaculating the lcp array, which is done in
+            O(n+m), where n is the size of the original string and m is
+            the size of substr.
+        """
+        start_pos = len(self.str)
+        self.str += substr
+        end_pos = len(self.str)
+        for i in range(start_pos, end_pos):
+            self.suffix_array.insert(self.find_SA_pos(self.str[i:]), i)
+        self.suffix_array.sort(self.__sarray_sort)
+        self.derive_lcp_array()
+
+    # Search functions
+
     def search(self, target):
         """ Searches for a substring in string using SA and binary search on prefixes, returns first instance """
         SA_pos = self.search_SA(target)
@@ -88,6 +123,31 @@ class SuffixArray:
             else:
                 hi = middle
         return -1  # not found
+
+#    def search_forward(self, target, pos):
+#        """ Searches the suffix array for a substring using binary search on prefixes, returns first instance
+#            Similar to search_SA, but it searches forwards from the given position in the original string
+#        """
+#        lo = 0
+#        hi = len(self.str)
+#        while hi > lo:
+#            middle = (lo + hi) / 2
+#            if self.get_pos(middle) <= pos:
+#                if target == self.get_sarray_item(middle+1)[0:len(target)]:
+#                    return self.get_pos(middle+1)
+#                elif target > self.get_sarray_item(middle+1)[0:len(target)]:
+#                    lo = middle + 2
+#                else:
+#                    hi = middle - 1
+#            else:
+#                if target == self.get_sarray_item(middle)[0:len(target)]:
+#                    return self.get_pos(middle)
+#                elif target > self.get_sarray_item(middle)[0:len(target)]:
+#                    lo = middle + 1
+#                else:
+#                    hi = middle
+#
+#        return None  # not found
 
     def search_all(self, target):
         """ Searches the suffix array for substring using binary search on prefixes, returns all instances """
@@ -117,25 +177,6 @@ class SuffixArray:
                     break
         return positions
 
-    def get_pos(self, pos):
-        """ Returns the position of the suffix array element in the original string """
-        return len(self.str) - len(self.get_sarray_item(pos))
-
-    def get_sarray_item(self, i):
-        """ Returns row from suffix array at position i """
-        return self.str[self.suffix_array[i]:]
-
-    def get_sarray_item_len(self, i):
-        """ Returns row length from suffix array at position i """
-        return len(self.str[self.suffix_array[i]:])
-
-    def get_suffix_array(self):
-        """ Returns the suffix array """
-        tmp_array = []
-        for i in self.suffix_array:
-            tmp_array.append(self.str[i:])
-        return tmp_array
-
     def find_SA_pos(self, target):
         """ Searches for sorted position of target within Suffix Array """
         lo = 0
@@ -157,16 +198,36 @@ class SuffixArray:
         else:
             return lo
 
-    def add_to_suffix_array(self, substr):
-        """ Adds a substring to the suffix array. Requires resorting the
-            suffix array and recaculating the lcp array, which is done in
-            O(n+m), where n is the size of the original string and m is
-            the size of substr.
-        """
-        start_pos = len(self.str)
-        self.str += substr
-        end_pos = len(self.str)
-        for i in range(start_pos, end_pos):
-            self.suffix_array.insert(self.find_SA_pos(self.str[i:]), i)
-        self.suffix_array.sort(self.__sarray_sort)
-        self.derive_lcp_array()
+    #def find_common_substrs(self):
+    #    """ Searches for the common substrings through the original string """
+    #    strings_found = []
+    #    start_substr = 0
+    #    length = 0
+    #    found_pos = None
+    #    i = 0
+    #    search_str = self.str[i]
+    #    while True:
+    #        pos = self.search_forward(search_str, start_substr)
+    #        print search_str, pos
+    #        if pos:  # if extended string found, add one more character
+    #            found_pos = pos  # update last found position
+    #            length += 1      # update length of sub str at last found position
+    #            if i != len(self.str)-1:
+    #                i += 1           # move search pointer forward
+    #                search_str += self.str[i]
+    #            else:
+    #                break  # reached the end
+    #        else:  # search string not found
+    #            if length:  # No match and we reached end of a substring
+    #                strings_found.append((start_substr, found_pos, length))
+    #                length = 0       #  reset length of previously sound sub str
+    #            elif len(search_str) == 1:  # no search string found move to the next character
+    #                if i != len(self.str)-1:
+    #                    i += 1
+    #                else:
+    #                    break  # reached the end
+    #            search_str = self.str[i]  #  moving to next substring, reset search string
+    #            start_substr = i #  moving to next substring, reset exclusion position
+    #            found_pos = None         #  moving to next substring, reset found position
+    #
+    #    return strings_found
