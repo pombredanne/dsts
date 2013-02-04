07d20d5be1d018f7aee83157b133aa4a29b330bf
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2013-02-04 11:16:57 +1100

    added left suffix extension for Reverse SA

--- dsts/suffix_array.py
+++ dsts/suffix_array.py
@@ -3,17 +3,19 @@
 # --------------------------------------------------------------------------
 # Description: Suffix array implementation. Provides functionality to search
 #              it and find all substring duplicates in the provided string
-# Author: Angelos Molfetas (2012)
+# Author: Angelos Molfetas (2012, 2013)
 # Licence: BSD Licence, see attached LICENCE file
 # ---------------------------------------------------------------------------
 
 from pprint import pprint
+from operator import itemgetter
 
 
 class SuffixArray:
+    """ Suffix Array """
     def __init__(self, string):
         """ Constructor, builds and sorts the array
-        string: string to process, use only with memory/save operation
+        string: string to process
         """
         self.str = string
         self.generate_suffix_array()
@@ -25,28 +27,32 @@ class SuffixArray:
     def generate_suffix_array(self):
         """ Generates the suffix and lcp array """
         self.suffix_array = range(len(self.str))
-        self.suffix_array.sort(self.__sarray_sort)
+        self.suffix_array.sort(self._sarray_sort)
         self.derive_lcp_array()
 
     def derive_lcp_array(self):
         """ Derive lcp array """
         self.lcp_array = [-1, ]
         for i in range(len(self.suffix_array) - 1):
-            string1 = self.str[self.suffix_array[i]:]
-            string2 = self.str[self.suffix_array[i + 1]:]
-            count = 0
-            for i in range(len(string1)):
-                if string1[i] == string2[i]:
-                    count += 1
-                else:
-                    break
-            self.lcp_array.append(count)
+            string1 = self.str[self.get_pos(i):]
+            string2 = self.str[self.get_pos(i + 1):]
+            self.lcp_array.append(self.compare_strings(string1, string2))
+
+    def compare_strings(self, string1, string2):
+        """ Compares two strings left to right, and returns how characters matched """
+        count = 0
+        for i in range(len(string1)):
+            if string1[i] == string2[i]:
+                count += 1
+            else:
+                break
+        return count
 
     def get_lcp_array(self):
         """ Return Long Common Prefix array """
         return self.lcp_array
 
-    def __sarray_sort(self, x, y):
+    def _sarray_sort(self, x, y):
         """ Sort two integers by comparing substrings in document string """
         if self.str[x:] > self.str[y:]:
             return 1
@@ -68,24 +74,25 @@ class SuffixArray:
 
     def get_pos(self, pos):
         """ Returns the position of the suffix array element in the original string """
-        return len(self.str) - len(self.get_sarray_item(pos))
+        return self.suffix_array[pos]
+        #return len(self.str) - len(self.get_sarray_item(pos))
 
     def get_sarray_item(self, i):
         """ Returns row from suffix array at position i """
-        return self.str[self.suffix_array[i]:]
+        return self.str[self.get_pos(i):]
 
     def get_sarray_item_len(self, i):
         """ Returns row length from suffix array at position i """
-        return len(self.str[self.suffix_array[i]:])
+        return len(self.str[self.get_pos(i):])
 
     def get_suffix_array(self):
         """ Returns the suffix array """
         tmp_array = []
-        for i in self.suffix_array:
-            tmp_array.append(self.str[i:])
+        for i in range(len(self.suffix_array)):
+            tmp_array.append(self.str[self.get_pos(i):])
         return tmp_array
 
-    def add_to_suffix_array(self, substr):
+    def add_to_suffix_array_right(self, substr):
         """ Adds a substring to the suffix array. Requires resorting the
             suffix array and recaculating the lcp array, which is done in
             O(n+m), where n is the size of the original string and m is
@@ -96,7 +103,7 @@ class SuffixArray:
         end_pos = len(self.str)
         for i in range(start_pos, end_pos):
             self.suffix_array.insert(self.find_SA_pos(self.str[i:]), i)
-        self.suffix_array.sort(self.__sarray_sort)
+        self.suffix_array.sort(self._sarray_sort)
         self.derive_lcp_array()
 
     # Search functions
@@ -190,7 +197,6 @@ class SuffixArray:
             elif target > self.get_sarray_item(middle):
                 lo = middle + 1
             else:
-                print target, self.get_sarray_item(middle)
                 hi = middle
 
         if target > self.get_sarray_item(middle):
@@ -231,3 +237,81 @@ class SuffixArray:
     #            found_pos = None         #  moving to next substring, reset found position
     #
     #    return strings_found
+
+    def find_longest_common_string_pairs(self):
+        """ Searches for the common substrings through the original string """
+
+        # Reserve memory for list to store pairs of strings
+        string_copies = [None] * len(self.lcp_array)
+        duplicates = {}  # Used to store identified string pairs and filter out smaller substrings. value = (offset, replica_offset, length)
+
+        for i in range(1, len(self.lcp_array)):
+            if self.lcp_array[i] > 0:
+                first_str = min(self.suffix_array[i], self.suffix_array[i - 1])
+                secon_str = max(self.suffix_array[i], self.suffix_array[i - 1])
+                length = self.lcp_array[i]
+                end_first_str = first_str + length
+                print i, (first_str, secon_str, length)
+                if end_first_str > secon_str:  # If suffixes are overlapping
+                    length = secon_str - first_str  # Reduce their length so they don't overlap
+                    end_first_str = first_str + length
+                print i, (first_str, secon_str, length), "*"
+                if end_first_str not in duplicates:  # if no value then add it
+                    duplicates[end_first_str] = (first_str, secon_str, length)
+                elif duplicates[end_first_str][2] < length:  # if length of previous value is smaller replace
+                    duplicates[end_first_str] = (first_str, secon_str, length)
+        print duplicates
+
+
+class ReverseSuffixArray(SuffixArray):
+    """ Suffix Array variant. Suffix Array offsets are instead stored right to left of the source string.
+        This means that translating positions in the suffix array into positions in the original string is slower
+        as a transform needs to be applied, however, inserting more characters into the suffix array is more efficient
+    """
+    def generate_suffix_array(self):
+        """ Generates the suffix and lcp array """
+        self.suffix_array = range(len(self.str) - 1, -1, -1)
+        self.suffix_array.sort(self._sarray_sort)
+        self.derive_lcp_array()
+
+    def get_pos(self, pos):
+        """ Returns the position of the suffix array element in the original string """
+        return (len(self.str) - 1) - self.suffix_array[pos]
+
+    def _sarray_sort(self, x, y):
+        """ Sort two integers by comparing substrings in document string """
+        x = (len(self.str) - 1) - x
+        y = (len(self.str) - 1) - y
+
+        if self.str[x:] > self.str[y:]:
+            return 1
+        elif self.str[x:] == self.str[y:]:
+            return 0
+        else:
+            return -1
+
+    def add_to_suffix_array_left(self, substr):
+        """ Adds a substring to the suffix array """
+        previous_size = len(self.str)
+        self.str = substr + self.str
+        for i in range(0, len(substr)):
+            # Add new suffix into SA
+            STR_pos = (len(self.str) - 1) - i
+            SA_pos = self.find_SA_pos(self.str[i:])
+            self.suffix_array.insert(SA_pos, STR_pos)
+            # Update LCP array
+            if SA_pos == 0:  # if it is in the beginning of the LCP array
+                self.lcp_array.insert(0, -1)  # Change 1st item to -1
+                string1 = self.str[self.get_pos(0):]
+                string2 = self.str[self.get_pos(1):]
+                self.lcp_array[1] = self.compare_strings(string1, string2)  # Update 2nd item from -1
+            elif SA_pos == (len(self.suffix_array) - 1):  # if at the end of the Suffix Array
+                string1 = self.str[self.get_pos(SA_pos):]
+                string2 = self.str[self.get_pos(SA_pos - 1):]
+                self.lcp_array.insert(SA_pos, self.compare_strings(string1, string2))
+            else:  # if it is inserted somewhere in the middle of the Suffix Array
+                string1 = self.str[self.get_pos(SA_pos):]
+                string2 = self.str[self.get_pos(SA_pos - 1):]
+                self.lcp_array.insert(SA_pos, self.compare_strings(string1, string2))
+                string2 = self.str[self.get_pos(SA_pos + 1):]
+                self.lcp_array[SA_pos + 1] = self.compare_strings(string1, string2)
--- tests/test_suffix_array.py
+++ tests/test_suffix_array.py
@@ -6,7 +6,7 @@
 # Licence: BSD Licence, see attached LICENCE file
 # ----------------------------------------------------------------
 
-from dsts.suffix_array import SuffixArray
+from dsts.suffix_array import SuffixArray, ReverseSuffixArray
 from nose.tools import assert_equal, raises
 from os import remove, makedirs
 from os.path import exists, dirname, realpath, exists
@@ -14,14 +14,15 @@ from sets import Set
 from pprint import pprint
 
 
-class TestSuffixArray:
+class TestRevSuffixArray:
+    """ Testing module for the Reverse Suffix Array """
     @classmethod
     def setup_class(self):
-        """ Initial configuration of TestSuffixArray, runs only once """
+        """ REV_SARRAY: Initial configuration of TestSuffixArray, runs only once """
         self.test_str = ["zabcada trip123", "abcadab", "abc123abc12abca"]
-        self.sarray1 = SuffixArray(string=self.test_str[0])
-        self.sarray2 = SuffixArray(string=self.test_str[1])
-        self.sarray3 = SuffixArray(string=self.test_str[2])
+        self.sarray1 = ReverseSuffixArray(string=self.test_str[0])
+        self.sarray2 = ReverseSuffixArray(string=self.test_str[1])
+        self.sarray3 = ReverseSuffixArray(string=self.test_str[2])
         self.sa_range = range(len(self.sarray1.suffix_array))
 
     @classmethod
@@ -30,7 +31,7 @@ class TestSuffixArray:
         pass
 
     def test_init(self):
-        """ Test initialisation of the Suffix Array """
+        """ REV_SARRAY: Test initialisation of the Suffix Array """
         assert_equal(self.sarray1.str, self.test_str[0])
         # Check that suffix array has been built properly
         test_array = []
@@ -39,7 +40,7 @@ class TestSuffixArray:
         assert_equal(self.sarray1.get_suffix_array(), sorted(test_array))
 
     def test_init_unicode(self):
-        """ Test initialisation of the Suffix Array using unicode string """
+        """ REV_SARRAY: Test initialisation of the Suffix Array using unicode string """
         test_str = unicode('abcdef') + unichr(200)
         sarray = SuffixArray(string=test_str)
         test_array = []
@@ -48,25 +49,25 @@ class TestSuffixArray:
         assert_equal(sarray.get_suffix_array(), sorted(test_array))
 
     def test_init_greek(self):
-        """ Test loading file with non standard ascii characters """
+        """ REV_SARRAY: Test loading file with non standard ascii characters """
         loc = dirname(realpath(__file__))
         f = open('%s/samples/Greek-Lipsum.txt' % loc, 'r')
         tmp_str = f.read()
         sarray = SuffixArray(string=tmp_str)
 
     def test_array_as_str(self):
-        """ Test returnining suffix array as a string """
+        """ REV_SARRAY: Test returnining suffix array as a string """
         tmp = ""
         for i in self.sa_range:
             tmp = tmp + "%s %s\n" % (i, self.sarray1.get_sarray_item(i))
         assert_equal(self.sarray1.return_array_as_string(), tmp)
 
     def test_return_original_string(self):
-        """ Test returning original string provided to constructor """
+        """ REV_SARRAY: Test returning original string provided to constructor """
         assert_equal(self.sarray1.return_original_str(), self.test_str[0])
 
     def test_search(self):
-        """ Search for substings """
+        """ REV_SARRAY: Search for substings """
         assert_equal(self.sarray1.search(' '), 7)     # search for a char within string
         assert_equal(self.sarray1.search('trip'), 8)  # search for a word within string
         assert_equal(self.sarray1.search('z'), 0)     # Search for character in the beginning
@@ -76,7 +77,7 @@ class TestSuffixArray:
         assert_equal(self.sarray1.search('y'), -1)    # Search for non existant character
 
     def test_multiple_search(self):
-        """ Search for all instances of a substring """
+        """ REV_SARRAY: Search for all instances of a substring """
         sa = SuffixArray(string='z123ABC123CBA256123')
         assert_equal(sa.search_all('ABC'), [4])
         assert_equal(sa.search_all('123'), [7, 1, 16])
@@ -84,13 +85,13 @@ class TestSuffixArray:
         assert_equal(sa.search_all('x'), [])
 
     def test_suffix_array_item(self):
-        """ Get suffix array items """
+        """ REV_SARRAY: Get suffix array items """
         array = ['ab', 'abcadab', 'adab', 'b', 'bcadab', 'cadab', 'dab']
         for i in range(len(array)):
             assert_equal(self.sarray2.get_sarray_item(i), array[i])
 
     def test_lcp_array(self):
-        """ Check validity of LCP array """
+        """ REV_SARRAY: Check validity of LCP array """
         string = "banana"
         lcp_array = [-1, 1, 3, 0, 0, 2]
         sarray = SuffixArray(string=string)
@@ -100,11 +101,156 @@ class TestSuffixArray:
         sarray = SuffixArray(string=string)
         assert_equal(lcp_array, sarray.get_lcp_array())
 
-    def test_adding_to_suffix_array(self):
-        """ Checks addings a substring to the suffix array """
-        x = SuffixArray("ABCAB")
-        x.add_to_suffix_array('Z')
+    def test_adding_to_suffix_array_right(self):
+        """ REV_SARRAY: Adding a substring to the right of the suffix array """
+        x = ReverseSuffixArray("ABCAB")
+        x.add_to_suffix_array_right('Z')
         SA = ['ABCABZ', 'ABZ', 'BCABZ', 'BZ', 'CABZ', 'Z']
         assert_equal(SA, x.get_suffix_array())
         LCP = [-1, 2, 0, 1, 0, 0]
+        assert_equal(LCP, x.get_lcp_array())
+        x.add_to_suffix_array_right('22')
+        SA = ['2', '22', 'ABCABZ22', 'ABZ22', 'BCABZ22', 'BZ22', 'CABZ22', 'Z22']
+        assert_equal(SA, x.get_suffix_array())
+        LCP = [-1, 1, 0, 2, 0, 1, 0, 0]
+        assert_equal(LCP, x.get_lcp_array())
+        x = ReverseSuffixArray("ABCAB")
+        x.add_to_suffix_array_right('AB')
+        SA = ['AB', 'ABAB', 'ABCABAB', 'B', 'BAB', 'BCABAB', 'CABAB']
+        assert_equal(SA, x.get_suffix_array())
+        LCP = [-1, 2, 2, 0, 1, 1, 0]
+        assert_equal(LCP, x.get_lcp_array())
+        x = ReverseSuffixArray("ABCAB")
+        x.add_to_suffix_array_right('BB')
+        SA = ['ABBB', 'ABCABBB', 'B', 'BB', 'BBB', 'BCABBB', 'CABBB']
+        assert_equal(SA, x.get_suffix_array())
+        LCP = [-1, 2, 0, 1, 2, 1, 0]
+        assert_equal(LCP, x.get_lcp_array())
+
+
+class TestSuffixArray:
+    """ Testsing module for the Suffix Array """
+    @classmethod
+    def setup_class(self):
+        """ SARRAY: Initial configuration of TestSuffixArray, runs only once """
+        self.test_str = ["zabcada trip123", "abcadab", "abc123abc12abca"]
+        self.sarray1 = SuffixArray(string=self.test_str[0])
+        self.sarray2 = SuffixArray(string=self.test_str[1])
+        self.sarray3 = SuffixArray(string=self.test_str[2])
+        self.sa_range = range(len(self.sarray1.suffix_array))
+
+    @classmethod
+    def teardown_class(self):
+        """ SARRAY: Finalise testing, runs only once at the end """
+        pass
+
+    def test_init(self):
+        """ SARRAY: Test initialisation of the Suffix Array """
+        assert_equal(self.sarray1.str, self.test_str[0])
+        # Check that suffix array has been built properly
+        test_array = []
+        for i in self.sa_range:
+            test_array.append(self.test_str[0][i:])
+        assert_equal(self.sarray1.get_suffix_array(), sorted(test_array))
+
+    def test_init_unicode(self):
+        """ SARRAY: Test initialisation of the Suffix Array using unicode string """
+        test_str = unicode('abcdef') + unichr(200)
+        sarray = SuffixArray(string=test_str)
+        test_array = []
+        for i in range(len(test_str)):
+            test_array.append(test_str[i:])
+        assert_equal(sarray.get_suffix_array(), sorted(test_array))
+
+    def test_init_greek(self):
+        """ SARRAY: Test loading file with non standard ascii characters """
+        loc = dirname(realpath(__file__))
+        f = open('%s/samples/Greek-Lipsum.txt' % loc, 'r')
+        tmp_str = f.read()
+        sarray = SuffixArray(string=tmp_str)
+
+    def test_array_as_str(self):
+        """ SARRAY: Test returnining suffix array as a string """
+        tmp = ""
+        for i in self.sa_range:
+            tmp = tmp + "%s %s\n" % (i, self.sarray1.get_sarray_item(i))
+        assert_equal(self.sarray1.return_array_as_string(), tmp)
+
+    def test_return_original_string(self):
+        """ SARRAY: Test returning original string provided to constructor """
+        assert_equal(self.sarray1.return_original_str(), self.test_str[0])
+
+    def test_search(self):
+        """ SARRAY: Search for substings """
+        assert_equal(self.sarray1.search(' '), 7)     # search for a char within string
+        assert_equal(self.sarray1.search('trip'), 8)  # search for a word within string
+        assert_equal(self.sarray1.search('z'), 0)     # Search for character in the beginning
+        assert_equal(self.sarray1.search('za'), 0)    # Search for 2 characters in the beginning
+        assert_equal(self.sarray1.search('3'), 14)    # Seach for character in the end
+        assert_equal(self.sarray1.search('123'), 12)  # Search for many characters in the end
+        assert_equal(self.sarray1.search('y'), -1)    # Search for non existant character
+
+    def test_multiple_search(self):
+        """ SARRAY: Search for all instances of a substring """
+        sa = SuffixArray(string='z123ABC123CBA256123')
+        assert_equal(sa.search_all('ABC'), [4])
+        assert_equal(sa.search_all('123'), [7, 1, 16])
+        assert_equal(sa.search_all('z'), [0])
+        assert_equal(sa.search_all('x'), [])
+
+    def test_suffix_array_item(self):
+        """ SARRAY: Get suffix array items """
+        array = ['ab', 'abcadab', 'adab', 'b', 'bcadab', 'cadab', 'dab']
+        for i in range(len(array)):
+            assert_equal(self.sarray2.get_sarray_item(i), array[i])
+
+    def test_lcp_array(self):
+        """ SARRAY: Check validity of LCP array """
+        string = "banana"
+        lcp_array = [-1, 1, 3, 0, 0, 2]
+        sarray = SuffixArray(string=string)
+        assert_equal(lcp_array, sarray.get_lcp_array())
+        string = "abc12abc15"
+        lcp_array = [-1, 1, 0, 0, 0, 4, 0, 3, 0, 2]
+        sarray = SuffixArray(string=string)
+        assert_equal(lcp_array, sarray.get_lcp_array())
+
+    def test_adding_to_suffix_array_right(self):
+        """ SARRAY: Adding a substring to the right of the suffix array """
+        x = SuffixArray("ABCAB")
+        x.add_to_suffix_array_right('Z')
+        SA = ['ABCABZ', 'ABZ', 'BCABZ', 'BZ', 'CABZ', 'Z']
+        assert_equal(SA, x.get_suffix_array())
+        LCP = [-1, 2, 0, 1, 0, 0]
+        assert_equal(LCP, x.get_lcp_array())
+        x.add_to_suffix_array_right('22')
+        SA = ['2', '22', 'ABCABZ22', 'ABZ22', 'BCABZ22', 'BZ22', 'CABZ22', 'Z22']
+        assert_equal(SA, x.get_suffix_array())
+        LCP = [-1, 1, 0, 2, 0, 1, 0, 0]
+        assert_equal(LCP, x.get_lcp_array())
+        x = ReverseSuffixArray("ABCAB")
+        x.add_to_suffix_array_right('AB')
+        SA = ['AB', 'ABAB', 'ABCABAB', 'B', 'BAB', 'BCABAB', 'CABAB']
+        assert_equal(SA, x.get_suffix_array())
+        LCP = [-1, 2, 2, 0, 1, 1, 0]
+        assert_equal(LCP, x.get_lcp_array())
+        x = ReverseSuffixArray("ABCAB")
+        x.add_to_suffix_array_right('BB')
+        SA = ['ABBB', 'ABCABBB', 'B', 'BB', 'BBB', 'BCABBB', 'CABBB']
+        assert_equal(SA, x.get_suffix_array())
+        LCP = [-1, 2, 0, 1, 2, 1, 0]
+        assert_equal(LCP, x.get_lcp_array())
+
+    def test_adding_to_suffix_array_left(self):
+        """ SARRAY: Adding a substring to the left of the suffix array """
+        x = ReverseSuffixArray("ABCAB")
+        x.add_to_suffix_array_left('X')
+        SA = ['AB', 'ABCAB', 'B', 'BCAB', 'CAB', 'XABCAB']
+        LCP = [-1, 2, 0, 1, 0, 0]
+        assert_equal(SA, x.get_suffix_array())
+        assert_equal(LCP, x.get_lcp_array())
+        x.add_to_suffix_array_left('AP')
+        SA = ['AB', 'ABCAB', 'APXABCAB', 'B', 'BCAB', 'CAB', 'PXABCAB', 'XABCAB']
+        LCP = [-1, 2, 1, 0, 1, 0, 0, 0]
+        assert_equal(SA, x.get_suffix_array())
         assert_equal(LCP, x.get_lcp_array())
