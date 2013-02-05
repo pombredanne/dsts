787e6147580116be978490ca20b5599f21b33865
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2013-02-05 16:00:21 +1100

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="dsts",
-      version="0.3",
+      version="0.4",
       description="Python data structures.",
       author="Angelos Molfetas",
       author_email="angelos.molfetas@unimelb.edu.au",
