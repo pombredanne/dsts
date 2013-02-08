ec52beafe46e88c455efa9d3d3c9f0f29269ddd7
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2013-02-08 17:47:47 +1100

    Added quad solution to the super maximal repeats

--- dsts/search.py
+++ dsts/search.py
@@ -3,31 +3,37 @@
 # --------------------------------------------------------------------------
 # Description: Strings search utilities.
 # Author: Angelos Molfetas (2013)
+# Copyright: The University of Melbourne (2013)
 # Licence: BSD Licence, see attached LICENCE file
 # ---------------------------------------------------------------------------
 
 from dsts.suffix_array import ReverseSuffixArray
 
 
-def super_maximal_repeats(string, RSA=None):
-    """ Find super maximal repeats """
+def super_maximal_repeats(string, RSA=None, reverse=False):
+    """ Find super maximal repeats
+        :RSA: Use supplied Reverse Suffix Array
+        :reverse: Provide string locations numbered from left to right
+    """
     strings_found = []
-    i = len(string) - 2
+
     start_pos = len(string) - 2
     if RSA is None:
         SA = ReverseSuffixArray(string[-1])
+        i = len(string) - 2  # One character is needed to build starting SA, this character is skipped
         search_str = string[-2]
+        offset_adj = 0
     else:
         SA = RSA
-        i = i - len(RSA.return_original_str()) + 1
-        start_pos = i
-        search_str = string[i]
-    prev_found = 0
+        i = len(string) - 1  # Suffix array exists already, no need to skip character
+        offset_adj = len(RSA.return_original_str())  # i indexes 'string'. doesn't consider SA str, so we need to account for
+    start_pos = i  # Keep track of starting position of the string where a match was first detected
+    search_str = string[i]
+    prev_found = 0  # We need to keep track of the previous value of i where there was a match
     length = 0
-    print "-->", string
     while True:
         found = SA.search_reverse(search_str)
-        print SA.return_original_str(), search_str, i
+        # print SA.return_original_str(), search_str, i
         if found != -1:  # Found, move to next character
             if i > 0:
                 start_pos = i
@@ -36,8 +42,12 @@ def super_maximal_repeats(string, RSA=None):
                 length += 1
             else:  # If no next character then record last entry and exit
                 length += 1
-                strings_found.append((length, i, len(string) - 1 - found))
-                break
+                if reverse is False:
+                    strings_found.append((length, i, len(string) - 1 - found + offset_adj))
+                else:
+                    strings_found.append((length, len(string) - 1 - i + offset_adj, found))
+                SA.add_to_suffix_array_left(search_str)  # We need to add it in case we reuse the SA
+                break  # Finished parsing, thus exit the loop
             search_str = string[i] + search_str
         else:  # Not found
             if len(search_str) == 1:  # Single character does not match, move to next character
@@ -48,10 +58,47 @@ def super_maximal_repeats(string, RSA=None):
                     break
                 start_pos = i
             else:  # There were previous characters that matched
-                strings_found.append((length, start_pos, len(string) - 1 - prev_found))
-                print "1-->", SA.return_original_str(), string
+                if reverse is False:
+                    strings_found.append((length, start_pos, len(string) - 1 - prev_found + offset_adj))
+                else:
+                    strings_found.append((length, len(string) - 1 - start_pos + offset_adj, prev_found))
                 SA.add_to_suffix_array_left(search_str[1:])
-                print "2-->", SA.return_original_str(), string
             search_str = string[i]
             length = 0
+    return strings_found
+
+
+def super_maximal_repeats_quad(string, reverse=None):
+    """ Identifies super maximal repeats incrementally from left to right (quandric complexity) """
+    strings_found = []
+    i = len(string) - 1
+    search_str = string[i]
+    prev_found = None
+    length = 0
+
+    while True:
+        found = string.find(search_str, 0, i)
+        print i, found, length, search_str
+        if found != -1:  # Found, move to next character
+            prev_found = found
+            start_pos = i
+            length += 1
+            if i > 0:
+                i -= 1
+            else:  # No further character, we have reached the end of the string
+                strings_found.append((length, start_pos, i))
+                length = 0
+                break
+            search_str = string[i] + search_str
+        else:  # Character not found
+            if len(search_str) == 1:  # Single character does not match, move to next character
+                if i > 1:
+                    i -= 1
+                else:
+                    break
+                start_pos = i
+            else:  # There were previous characters that matched
+                strings_found.append((length, start_pos, prev_found))
+                length = 0
+            search_str = string[i]
     return strings_found
--- tests/test_search.py
+++ tests/test_search.py
@@ -19,6 +19,7 @@ class Test_Search:
         """ SEARCH: Find super maximal repeats by parsing file into a reverse suffix array """
         x = super_maximal_repeats('ABCAB')
         answer = [(2, 0, 3)]
+        assert_equal(x, answer)
         x = super_maximal_repeats('ABCABC')
         answer = [(3, 0, 3)]
         assert_equal(x, answer)
@@ -33,8 +34,41 @@ class Test_Search:
         assert_equal(x, answer)
 
     def test_super_maximal_repeats_pass_RSA(self):
-        """ SEARCH: Find super maximal repeats by parsing file into a provided reverse suffix array """
+        """ SEARCH: Find Super maximal repeats by parsing file into a provided reverse suffix array """
         RSA = ReverseSuffixArray('ABC')
         x = super_maximal_repeats('ABCABCABC', RSA)
-        answer = [(3, 3, 6), (3, 0, 3)]
+        answer = [(3, 6, 9), (6, 0, 6)]
         assert_equal(x, answer)
+
+    def test_super_maximal_repeats_pass_reverse(self):
+        """ SEARCH: Find super maximal repeats by parsing file into a reverse suffix array. Reverse indexes """
+        x = super_maximal_repeats('ABCAB', reverse=True)
+        answer = [(2, 4, 1)]
+        assert_equal(x, answer)
+        x = super_maximal_repeats('ABCABC', reverse=True)
+        answer = [(3, 5, 2)]
+        assert_equal(x, answer)
+        x = super_maximal_repeats('ABCABCABC', reverse=True)
+        answer = [(3, 5, 2), (3, 8, 5)]
+        assert_equal(x, answer)
+        x = super_maximal_repeats('DCBABCCDCBA', reverse=True)
+        answer = [(1, 4, 2), (1, 5, 2), (1, 6, 1), (4, 10, 3)]
+        assert_equal(x, answer)
+        x = super_maximal_repeats('ABCABCABCABC', reverse=True)
+        answer = [(3, 5, 2), (6, 11, 5)]
+        assert_equal(x, answer)
+
+    def test_super_maximal_repeats_pass_rsa_and_reverse(self):
+        """ SEARCH: Find super maximal repeats. Reverse indexes, provide reverse suffix array """
+        SA = ReverseSuffixArray('C')
+        x = super_maximal_repeats('AB', RSA=SA, reverse=True)
+        answer = []
+        assert_equal(x, answer)
+        x = super_maximal_repeats('ABC', RSA=SA, reverse=True)
+        answer = [(3, 5, 2)]
+        assert_equal(x, answer)
+        x = super_maximal_repeats('ABC', RSA=SA, reverse=True)
+        answer = [(3, 8, 5)]
+        assert_equal(x, answer)
+        x = super_maximal_repeats('BCABCAB', RSA=SA, reverse=True)
+        answer = [(7, 15, 8)]
