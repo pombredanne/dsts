0d3ac84c47994469790bfc0cd9532d2650896ab9
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2013-03-22 12:08:45 +1100

    Added dsts c++ module as git submodule

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, Extension
+
+module1 = Extension('hello', sources=['dsts/lzmodule.cpp'])
 
 setup(name="dsts",
       version="0.5",
@@ -7,4 +9,6 @@ setup(name="dsts",
       author="Angelos Molfetas",
       author_email="angelos.molfetas@unimelb.edu.au",
       packages=['dsts'],
-      long_description="Python data structures. At the moment it only has a suffix array and a Rabin and Karp fingerprint generator.")
+      long_description="Python data structures. At the moment it only has a suffix array and a Rabin and Karp fingerprint generator.",
+      ext_modules=[module1],
+      )
