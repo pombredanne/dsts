1ade0af761b59c22c6111c995b8d3f46557e58c8
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2013-04-05 11:48:41 +1100

    interfaced lz c++ module

---
+++ dsts/lzmodule.cpp
@@ -0,0 +1,60 @@
+/*
+------------------------------------------------------------------
+ Description: LZ factorisor module, python bindings for lzOG C++
+              module.
+ Author: Angelos Molfetas (2013)
+ Copyright: The University of Melbourne (2013)
+ Licence: BSD licence, see attached LICENCE file
+ -----------------------------------------------------------------
+*/
+
+#include <Python.h>
+#include "external/dstsc/lzOG/src/lzOG.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static PyObject* factorise(PyObject* self, PyObject* args)
+{
+    char* str;
+    int numofbytes; // string lenght
+
+    if (!PyArg_ParseTuple(args, "s#", &str, &numofbytes))
+        return NULL;
+
+    vector<LONGINT> offsets; // Store the factor offsets
+    vector<LONGINT> lengths; // Store the length offsets
+
+    lz_factorise(numofbytes+1, (unsigned char*) str, offsets, lengths);
+
+    // Define tuple with LZ factors to return to the interpretter
+    PyObject* tuple = PyTuple_New(offsets.size()-1);
+    for(int i = 0; i < offsets.size() - 1; i++) // Print the lz factors
+       if (lengths[i] != 0)
+       {
+           cout << "(" << offsets[i] << "," << lengths[i] << ")" << endl;
+	   PyTuple_SetItem(tuple, i, Py_BuildValue("ii", offsets[i], lengths[i]));
+       }
+    
+       else  // when length is zero we are storing a char instead of an offset
+       {
+           cout << "(" << (unsigned char)offsets[i] << "," << lengths[i] << ")" << endl;
+	   PyTuple_SetItem(tuple, i, Py_BuildValue("ci", (unsigned char)offsets[i], lengths[i]));
+       }
+
+    return tuple;
+}
+ 
+static PyMethodDef LzMethods[] =
+{
+     {"factorise", factorise, METH_VARARGS, "Greet somebody."},
+     {NULL, NULL, 0, NULL}
+};
+ 
+PyMODINIT_FUNC
+ 
+initlz(void)
+{
+     (void) Py_InitModule("lz", LzMethods);
+}
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #from distutils.core import setup
 from setuptools import setup, Extension
 
-module1 = Extension('hello', sources=['dsts/lzmodule.cpp'])
+lz = Extension('lz', include_dirs=['dsts/external/dstc/lzOG/src/', 'dsts/external/dstc/SAIS-SK/src/'],
+               sources=['dsts/lzmodule.cpp', 'dsts/external/dstsc/lzOG/src/lzOG.cpp', 'dsts/external/dstsc/SAIS-SK/src/mmap.cpp',
+                        'dsts/external/dstsc/SAIS-SK/src/fileopen.cpp', 'dsts/external/dstsc/SAIS-SK/src/gt-alloc.cpp',
+                        'dsts/external/dstsc/SAIS-SK/src/sk-sain.cpp'])
 
 setup(name="dsts",
-      version="0.5",
+      version="0.6",
       description="Python data structures.",
       author="Angelos Molfetas",
       author_email="angelos.molfetas@unimelb.edu.au",
       packages=['dsts'],
-      long_description="Python data structures. At the moment it only has a suffix array and a Rabin and Karp fingerprint generator.",
-      ext_modules=[module1],
+      long_description="Python data structures. Suffix array Construction Algorithm, Rabin & Karp fingerprint generator, LZ factorisor.",
+      ext_modules=[lz],
       )
