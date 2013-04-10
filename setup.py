cdf97b658d00966f5a5504740fc2c306b896a50d
Angelos Molfetas <angelos.molfetas@unimelb.edu.au>
2013-04-10 12:00:18 +1000

    setup script will automatically pull submodules

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #from distutils.core import setup
 from setuptools import setup, Extension
+from os.path import exists
+from subprocess import call
 
 lz = Extension('dsts.lz', include_dirs=['dsts/external/dstc/lzOG/src/', 'dsts/external/dstc/SAIS-SK/src/'],
                sources=['dsts/lzmodule.cpp', 'dsts/external/dstsc/lzOG/src/lzOG.cpp', 'dsts/external/dstsc/SAIS-SK/src/mmap.cpp',
                         'dsts/external/dstsc/SAIS-SK/src/fileopen.cpp', 'dsts/external/dstsc/SAIS-SK/src/gt-alloc.cpp',
                         'dsts/external/dstsc/SAIS-SK/src/sk-sain.cpp'])
+
+# When this script is run, we want to unsure that the distc package is installed
+# The distc package has C++ code that is required when building the lz extension
+if not exists("dsts/external/dstsc/SAIS-SK/") or not exists("dsts/external/dstsc/lzOG/"):
+    call(["git", "submodule", "foreach", "git", "pull"])
 
 setup(name="dsts",
       version="0.6",
